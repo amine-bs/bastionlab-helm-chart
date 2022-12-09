8e145bfdc8bab3c2ff633a3a7522f7384eb0fa5d
amine-bs <aminesalah480@gmail.com>
2022-12-09 09:40:17 +0100

    add utils

---
+++ utils/generate_keys.py
@@ -0,0 +1,9 @@
+from keys import SigningKey
+
+data_owner_key = SigningKey.generate()
+data_owner_key.save_pem("privkeys/data_owner.key.pem")
+data_owner_pub_key = data_owner_key.pubkey.save_pem("pubkeys/data_owner.pem")
+
+data_scientist_key = SigningKey.generate()
+data_scientist_key.save_pem("privkeys/data_scientist.key.pem")
+data_scientist_pub_key = data_scientist_key.pubkey.save_pem("pubkeys/data_scientist.pem")
