ca0289847ee5d01ac5f636695613bf2ae453fdf4
amine-bs <aminesalah480@gmail.com>
2022-12-09 10:02:12 +0100

    edit utils

--- utils/generate_keys.py
+++ utils/generate_keys.py
@@ -5,5 +5,5 @@ data_owner_key.save_pem("/home/privkeys/data_owner.key.pem")
 data_owner_pub_key = data_owner_key.pubkey.save_pem("/home/owners/data_owner.pem")
 
 data_scientist_key = SigningKey.generate()
-data_scientist_key.save_pem("home/privkeys/data_scientist.key.pem")
-data_scientist_pub_key = data_scientist_key.pubkey.save_pem("home/users/data_scientist.pem")
+data_scientist_key.save_pem("/home/privkeys/data_scientist.key.pem")
+data_scientist_pub_key = data_scientist_key.pubkey.save_pem("/home/users/data_scientist.pem")
