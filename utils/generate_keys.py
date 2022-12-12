b87e8f6853aae8897b0bd0a697756f8ad5714806
amine-bs <aminesalah480@gmail.com>
2022-12-12 10:32:07 +0100

    edit chart

--- utils/generate_keys.py
+++ utils/generate_keys.py
@@ -1,9 +1,17 @@
 from keys import SigningKey
+import sys 
 
-data_owner_key = SigningKey.generate()
-data_owner_key.save_pem("/home/privkeys/data_owner.key.pem")
-data_owner_pub_key = data_owner_key.pubkey.save_pem("/home/owners/data_owner.pem")
+def main(args):
+    users_number = int(args[0])
+    data_owner_key = SigningKey.generate()
+    data_owner_key.save_pem("home/privkeys/data_owner.key.pem")
+    data_owner_pub_key = data_owner_key.pubkey.save_pem("home/owners/data_owner.pem")
+    for i in range(users_number):
+        data_scientist_key = SigningKey.generate()
+        data_scientist_key.save_pem("home/privkeys/data_scientist{}.key.pem".format(i+1))
+        data_scientist_pub_key = data_scientist_key.pubkey.save_pem("home/users/data_scientist{}.pem".format(i+1))
 
-data_scientist_key = SigningKey.generate()
-data_scientist_key.save_pem("/home/privkeys/data_scientist.key.pem")
-data_scientist_pub_key = data_scientist_key.pubkey.save_pem("/home/users/data_scientist.pem")
+
+
+if __name__=="__main__":
+    main(sys.argv[1:])
